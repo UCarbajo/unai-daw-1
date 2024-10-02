c0220e447baf3b4812b9b2ddbedea588a275f656
Unai <unai.carbajo.amundarain@gmail.com>
2024-10-02 16:22:12 +0200

    Ejercicio arrays

--- Workspace/ejercicio6/src/arrays/Main.java
+++ Workspace/ejercicio6/src/arrays/Main.java
@@ -5,13 +5,18 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Scanner entrada = new Scanner(System.in);
-		String[] mes = {"","Enero", "Febrero","Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Novimebre", "Diciembre" };
-		int numeroMes = entrada.nextInt();
-		
-		System.out.println(mes[numeroMes]);
-		}
 
+		Scanner entrada = new Scanner(System.in);
+		String[] mes = { "", "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre",
+				"Octubre", "Noviembre", "Diciembre" };
+		System.out.println("Introduce el numero de mes:");
+		int numeroMes = entrada.nextInt();
+
+		if (numeroMes > 0 && numeroMes <= 12) {
+			System.out.println(mes[numeroMes]);
+		} else {
+			System.out.println("Mes incorrecto. Introduzca un numero entre el 1-12");
+		}
 	}
 
+}
