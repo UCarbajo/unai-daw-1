29ec1aa110a3e279b4a661884b875b740f2791df
Unai <unai.carbajo.amundarain@gmail.com>
2024-10-22 22:41:20 +0200

    Pruebas personales

--- Programacion Alberto/Juego de la vida/Pruebas personales/01Pruebas_Metodos/src/Main.java
+++ Programacion Alberto/Juego de la vida/Pruebas personales/01Pruebas_Metodos/src/Main.java
@@ -1,25 +1,42 @@
 import java.util.Scanner;
 
 public class Main {
-    static void mostrarTabla(int[][] miTabla) {
-    	for (int i = 0; i < miTabla.length; i++) {
-    		for (int j = 0; j < miTabla[i].length; j++) {
-    			System.out.print(miTabla[i][j]);
-        	}
-    		System.out.println();
-    	}
-    }
-	
 	public static void main(String[] args) {
-    	Scanner entrada = new Scanner(System.in);
-    	
-    	System.out.println("Introduce el numero de filas deseada");
-    	int numeroFilas = entrada.nextInt();
-    	System.out.println("Introduce el numero de columnas deseada");
-    	int numeroColumnas = entrada.nextInt();
-    	
-    	int[][] tabla = new int[numeroFilas][numeroColumnas];
-    	
-    	mostrarTabla(tabla);
-    }
+		Scanner entrada = new Scanner(System.in);
+		
+		int numero1 = 0;
+		int numero2 = 0;
+		String tipoFuncion;
+		
+		System.out.println("Introduce el primer numero");
+		numero1 = entrada.nextInt();
+		System.out.println("Introduce el segundo numero");
+		numero2 = entrada.nextInt();
+		System.out.println("Introduce la funcion");
+		tipoFuncion = entrada.next();
+		
+		while(!"+".equals(tipoFuncion) && !"-".equals(tipoFuncion) && !"*".equals(tipoFuncion) && !"/".equals(tipoFuncion) ) {
+			System.out.println("Introduce un valor correcto (+, -, *, /)");
+			tipoFuncion = entrada.next();
+		}
+		
+		calculadora(numero1, numero2, tipoFuncion);
+	}
+	
+	static void calculadora(int primerNumero, int segundoNumero, String tipoFuncion) {
+		switch(tipoFuncion){
+		case "+":
+			System.out.println(primerNumero+segundoNumero);
+			break;
+		case "-":
+			System.out.println(primerNumero-segundoNumero);
+			break;
+		case "*":
+			System.out.println(primerNumero*segundoNumero);
+			break;
+		case "/":
+			System.out.println(primerNumero/segundoNumero);
+			break;
+		}
+	}
 }
