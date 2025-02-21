7bb685405b66120c815e77b81fa69aeb683a01f5
unai <unai>
2025-02-21 12:51:58 +0100

    programación

--- Programacion Alberto/Workspace_Reto3/01primeraWEB/src/main/java/com/centrosanluis/controller/Login.java
+++ Programacion Alberto/Workspace_Reto3/01primeraWEB/src/main/java/com/centrosanluis/controller/Login.java
@@ -1,6 +1,9 @@
 package com.centrosanluis.controller;
 
 import java.io.IOException;
+import java.util.HashMap;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,14 +17,35 @@ import javax.servlet.http.HttpServletResponse;
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	private HashMap<String, String> users = new HashMap<String, String>();
+	
+	public Login() {
+		super();
+	}
+
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		
+		users.put("admin", "1234");
+		users.put("usuario", "password");
+	}
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Estoy entrando por GET");
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Estoy entrando por POST");
-		System.out.println(request.getParameter("username"));
-		System.out.println(request.getParameter("password"));
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		if(users.containsKey(username) && users.get(username).equals(password)) {
+			response.sendRedirect("index.html");
+		}else {
+			response.sendRedirect("login.jsp?error=1");
+		}
 	}
 
 }
--- Programacion Alberto/Workspace_Reto3/01primeraWEB/src/main/webapp/login.jsp
+++ Programacion Alberto/Workspace_Reto3/01primeraWEB/src/main/webapp/login.jsp
@@ -1,3 +1,7 @@
+<!-- DIRECTIVA %@ MODIFICAR COMPORTAMIENTO DE LA PÁGINA-->
+<!-- DECLARACIONES %! VARIABLES/METODOS GLOBALES -->
+<!-- EXPRESIONES%= MOSTRAR UN VALOR JAVA -->
+<!-- CODIGO JAVA % PODEMOS AÑADIR CUALQUIER CODIGO JAVA -->
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
@@ -9,12 +13,17 @@
 <body>
 	<div>
 		<h2>Iniciar Sesión</h2>
-		<form action="login" method="get">
+		<form action="login" method="post">
 			<input type="text" name="username" placeholder="Nombre de Usuario">
 			<input type="password" name="password" placeholder="Contraseña">
 			<button type="submit">Entrar</button>
 			<button type="reset">Borrar</button>
 		</form>
+		<% if(request.getParameter("error") != null) {%>
+		<p>
+			Usuario o contraseña incorrecto.
+		</p>
+		<%} %>
 	</div>
 </body>
 </html>
