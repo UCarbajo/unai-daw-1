2b61c6401e2f836268e97b0b6cedbe412ba43529
unai <unai>
2025-02-25 13:48:22 +0100

    programacion

--- Programacion Alberto/Workspace_Reto3/01primeraWEB/src/main/java/com/centrosanluis/baseDatos/WebBD.java
+++ Programacion Alberto/Workspace_Reto3/01primeraWEB/src/main/java/com/centrosanluis/baseDatos/WebBD.java
@@ -3,7 +3,7 @@ package com.centrosanluis.baseDatos;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-import com.centrosanluis.controller.Usuario;
+import com.centrosanluis.model.Usuario;
 import com.mysql.cj.xdevapi.PreparableStatement;
 
 public class WebBD extends AccesoBD {
@@ -32,5 +32,42 @@ public class WebBD extends AccesoBD {
 		}
 		return existeUsuario;
 
+	}
+
+	public boolean anadirUsuario(Usuario user) {
+		//TODO PREPARAMOS UN BOOLEAN EN FALSO PARA INDICAR SI SE HA AÑADIDO O NO UN USUARIO
+		boolean usuarioAnadido = false;
+		//TODO PREPARAMOS UNA CONSULTA PARA COMPROBAR SI EXISTE ALGUIEN CON EL MISMO NOMBRE DE USUARIO
+		String sql = "SELECT * FROM usuarios WHERE username = ? OR mail = ?";
+		try {
+			conectar();
+			//TODO EXECUTAMOS LA CONSULTA Y LO RECIBE EL RESULTSET(rs)
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, user.getUserName());
+			ps.setString(2, user.getMail());
+			rs = ps.executeQuery();
+			//TODO SI EL RESULTSET NO CONTIENE UNA LINEA (NO HAY OTRO USUARIO O CORREO CON EL MISMO NOMBRE) 
+			//PREPARAMOS LA INSERCCION DE UN NUEVO USUARIO A LA BASE DE DATOS
+			//TAMBIEN CAMBIAMOS EL ESTADO DEL BOOLEAN PARA INDICAR QUE SE HA AÑADIDO UN USUARIO
+			if(!rs.next()) {
+				sql = "INSERT INTO usuarios(name, lastname, mail, username, password, phonenumber) VALUES (?, ?, ?, ?, ?, ?)";
+				ps = con.prepareStatement(sql);
+				ps.setString(1, user.getName());
+				ps.setString(2, user.getLastName());
+				ps.setString(3, user.getMail());
+				ps.setString(4, user.getUserName());
+				ps.setString(5, user.getPassWord());
+				ps.setString(6, String.valueOf(user.getPhoneNumber()));
+				ps.execute();
+				
+				usuarioAnadido = true;
+			}
+			desconectar();
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//TODO DEVOLVEMOS EL BOOLEAN INDICANDO SI SE HA AÑADIDO(TRUE) O NO(FALSE) UN USUARIO
+		return usuarioAnadido;
 	}
 }
---
+++ Programacion Alberto/Workspace_Reto3/01primeraWEB/src/main/java/com/centrosanluis/controller/AltaUsuario.java
@@ -0,0 +1,71 @@
+package com.centrosanluis.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.centrosanluis.baseDatos.WebBD;
+import com.centrosanluis.model.Usuario;
+
+/**
+ * Servlet implementation class AltaUsuario
+ */
+@WebServlet("/altaUsuario")
+public class AltaUsuario extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private WebBD acceso = new WebBD();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO OBTENEMOS TODOS LOS DATOS DEL FORMULARIO DE ALTA USUARIO
+		// TODO FALTA CONTROLAR LOS ERRORES (CAMPOS NO RELLENADOS, CAMPOS MAL
+		// RELLENADOS, ...)
+		String name = request.getParameter("name");
+		String lastName = request.getParameter("lastName");
+		String phoneNumber = request.getParameter("phoneNumber");
+		String mail = request.getParameter("mail");
+		String userName = request.getParameter("userName");
+		String passWord = request.getParameter("passWord");
+
+		// TODO COMPROBAMOS QUE LOS CAMPOS ESTEN RELLENADOS
+		
+		try {
+			if (!name.isBlank() && !lastName.isBlank() && !phoneNumber.isBlank() && !mail.isBlank() && !userName.isBlank() && !passWord.isBlank()) {
+				// TODO CREAMOS UNA INSTANCIA DEL USUARIO CON LOS DATOS DEL FORMULARIO
+				
+				Usuario user = new Usuario(name, lastName, mail, userName, passWord, Integer.parseInt(phoneNumber));
+
+				// TODO HACEMOS UNA CONSULTA A LA BD, SI SE AÑADE EL USUARIO CORRECTAMENTE, SE
+				// REDIRIGE AL USUARIO A LA PAGINA INDEX
+				if (acceso.anadirUsuario(user)) {
+					response.sendRedirect("index.html");
+				} else {
+					// TODO SI EL USUARIO NO SE HA PODIDO AÑADIR, CREAMOS UN ERROR DE USUARIO PARA
+					// PODER INDICARLO EN ALTAUSUARIO.JSP
+					request.getSession().setAttribute("errorUsuario", "1");
+					response.sendRedirect("altaUsuario.jsp");
+				}
+			}else {
+				//TODO SI ALGUN CAMPO NO ESTA RELLENADO, LANZAMOS UN ERROR
+				request.getSession().setAttribute("errorCampo", "1");
+				response.sendRedirect("altaUsuario.jsp");
+			}
+		}catch(NumberFormatException e) {
+			// TODO SI INTRODUCE LETRAS EN EL CAMPO DE NUMERO DE TELEFONO
+			// CAZAMOS EL ERROR Y MOSTRAMOS MENSAJE DE ERROR
+			request.getSession().setAttribute("errorCampo", "1");
+			response.sendRedirect("altaUsuario.jsp");
+		}
+
+	}
+
+}
